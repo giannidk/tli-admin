c77d30f5049cd1f76d8cd58161f51480da086091
Giovanni Ambrosio <gam@reviso.com>
2018-12-13 17:47:35 +0100

    1: active language persisted in local storage

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-
+    
     this.props.initialize({
       languages: [
         { name: "English", code: "en" },
@@ -42,8 +42,10 @@ class App extends Component {
     });
   }
   
-  componentWillMount() {
+  componentDidMount() {
     this.props.fetchUser();
+    const activeLanguage = localStorage.getItem('language')
+    activeLanguage && this.props.setActiveLanguage(localStorage.getItem('language'))
   }
 
   render() {
--- src/components/main/language-toggle.jsx
+++ src/components/main/language-toggle.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavDropdown, MenuItem } from 'react-bootstrap'
 import { withLocalize } from "react-localize-redux";
+import * as localization from '../../utils/localization'
 
 const LanguageToggle = ({ languages, activeLanguage, setActiveLanguage }) => (
 	<NavDropdown eventKey={3} title={(activeLanguage && activeLanguage.name) || 'Language'} id="language-nav-dropdown">
 		{languages.map(lang => (
-			<MenuItem key={lang.code} onClick={() => setActiveLanguage(lang.code)}>
+			<MenuItem key={lang.code} onClick={() => localization.setActiveLanguage(lang.code, setActiveLanguage)}>
 				{lang.name}
 			</MenuItem>
 		))}
